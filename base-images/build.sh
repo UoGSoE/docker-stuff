a18de991d14a8bf4704dca05f65aa59c1e2c242c
Billy <william.allan@glasgow.ac.uk>
2019-04-26 12:06:56 +0100

    Refactor for more leverage of multistage docker builds

--- base-images/build.sh
+++ base-images/build.sh
@@ -12,11 +12,13 @@ echo "Logging to ${LOGFILE}"
 for VERSION in "${VERSIONS[@]}";
 do
     echo "Building ${VERSION}..."
-    docker build --pull --no-cache --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}" -f Dockerfile.base . >> "${LOGFILE}"
+    docker build --pull --no-cache --target=prod --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}" -f Dockerfile.base . >> "${LOGFILE}"
     echo "Pushing ${VERSION}..."
+    docker push "${BASE_NAME}":"${VERSION}" >> "${LOGFILE}"
 
     echo "Building ${VERSION}-ci..."
-    docker build --pull --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}-ci" -f Dockerfile.base-ci . >> "${LOGFILE}"
+    docker build --pull --target=ci --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}"-ci -f Dockerfile.base . >> "${LOGFILE}"
     echo "Pushing ${VERSION}-ci..."
+    docker push "${BASE_NAME}":"${VERSION}"-ci >> "${LOGFILE}"
 done
 
