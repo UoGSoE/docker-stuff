028ec82ef14f9e7db4e6029468bdfd7bf189194b
Billy <billybofh@gmail.com>
2019-07-04 15:04:48 +0100

    Updated base-image build script to use multi-arch buildx

--- base-images/build.sh
+++ base-images/build.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-set -e
 
 BASE_NAME="uogsoe/soe-php-apache"
 VERSIONS=( "7.1" "7.2" "7.3" )
-export DOCKER_BUILDKIT=1
-
+CMD="docker buildx build --pull --push --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7" 
 PNAME=`basename $0`
 LOGFILE=`mktemp /tmp/${PNAME}.XXXXXX` || exit 1
+export DOCKER_BUILDKIT=1
+
+docker buildx &> /dev/null
+if [ $? -ne 0 ]
+then
+  echo "Aborting."
+  echo "You need to have docker buildx available. See https://github.com/docker/buildx"
+  exit 1
+fi
+
+set -e
+
 echo "Logging to ${LOGFILE}"
 
 for VERSION in "${VERSIONS[@]}";
 do
     echo "Building ${VERSION}..."
-    docker build --pull --no-cache --target=prod --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}" -f Dockerfile.base . >> "${LOGFILE}"
-    echo "Pushing ${VERSION}..."
-    docker push "${BASE_NAME}":"${VERSION}" >> "${LOGFILE}"
+    "$CMD" --target=prod --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}" -f Dockerfile.base . >> "${LOGFILE}"
 
     echo "Building ${VERSION}-ci..."
-    docker build --pull --target=ci --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}"-ci -f Dockerfile.base . >> "${LOGFILE}"
-    echo "Pushing ${VERSION}-ci..."
-    docker push "${BASE_NAME}":"${VERSION}"-ci >> "${LOGFILE}"
+    "${CMD}" --target=ci --build-arg PHP_VERSION=${VERSION} -t "${BASE_NAME}":"${VERSION}"-ci -f Dockerfile.base . >> "${LOGFILE}"
 done
 
